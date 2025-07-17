0d85ecbdf99e04700501d12128c39ad37cf0e3fe
Aviral Gupta <aviralg2002@gmail.com>
2025-07-17 10:56:31 +0530

    Feat: Added terraform cloud

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform { 
+  cloud { 
+    
+    organization = "org-aviral" 
+
+    workspaces { 
+      name = "my-terraform-workspace-1" 
+    } 
+  } 
+}
+
 provider "google" {
   project     = "sacred-reactor-465517-q6"
   region      = "asia-south2"
